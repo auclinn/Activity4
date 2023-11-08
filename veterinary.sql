397834f863e1bc65c091bed0e2f7ea4e135c287a
Bernadette Awatin <badet.awatin@gmail.com>
2023-11-08 09:25:58 +0800

    added feat create table owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE veterinary_database;
+
+--owners table
+CREATE TABLE owners(
+    ownerid INT,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100),
+        PRIMARY KEY (ownerid)
+);
