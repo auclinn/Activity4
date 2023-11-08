72f14f1d689f6ca09089afa98eea3fdc06d7bde0
dan dainie perez <perezdandainiel@gmail.com>
2023-11-08 09:33:13 +0800

    feat added create table animals

--- veterinary.sql
+++ veterinary.sql
@@ -10,3 +10,16 @@ CREATE TABLE owners(
     email VARCHAR(100),
         PRIMARY KEY (ownerid)
 );
+
+CREATE TABLE animals(
+    animalid INT,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    dateofbirth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+    ownerid INT,
+        PRIMARY KEY (animalid),
+        FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
